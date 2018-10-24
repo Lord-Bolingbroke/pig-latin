30f0769d13b2811b9a1efc1d8e0c37406dd86ee8
Katrina Hockman and Noah Schooler <noah@>
2018-10-24 12:02:27 -0700

    add test code for if statements

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var vowels = ['a', 'e', 'i', 'o', 'u']
+// var consonants = ['b', 'c', 'd', 'f', 'g', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't']
+
+
+// function split() {
+//   var userInput = "I like ice cream";
+//   var splitUserInput = userInput.split(" ");
+//   console.log(splitUserInput)
+//   debugger;
+// }
+
+var userInput = "i"
+
+function returnWay(userInput) {
+  if (userInput === "i") {
+    var = ("iway")
+  }
+    return var;
+  // var way = [];
+  // for (var i = 0; i < userInput.length; i ++) {
+  //   if (userInput[i] === vowels) {
+  //     way.push(userInput[i])
+  //     way.concat("way")
+  //
+  //   }
+  // }
+
+}
