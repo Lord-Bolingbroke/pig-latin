b953689cb7ff8e75ecaaa5b6f54b406ada3816df
Katrina Hockman and Noah Schooler <noah@>
2018-10-24 15:07:35 -0700

    fix branching for word beginning with vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
+
 var vowels = ['a', 'e', 'i', 'o', 'u']
 
-var userInput =
+var userInput = "app"
 
 function returnWay(userInput) {
   var returnArray = [];
 
-
-
-
-    if (userInput === "A" || "E" || "I" || "O" || "U") {
-      returnArray.push([i]+ "way");
+  if ((userInput.split("")[0] === "a") || (userInput.split("")[0] === "e") || (userInput.split("")[0] === "i") || (userInput.split("")[0] === "o") || (userInput.split("")[0] === "u")) {
+      returnArray.push(userInput + "way");
     }
-  }
     return returnArray;
-
 }
+
+// function consonantShift(userInput) {
+//   var returnArray = [];
+//
+//   if (userInput !== "a" || "e" || "i" || "o" || "u") {
+//     returnArray.push(userInput.splice(0, 1, ))
+//   }
+//
+//
+//
+//
+// }
+//
+// if (userInput === "a") || (userInput === "e") || (userInput === "i")
