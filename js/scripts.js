cb4793a232607e48f55fe6a13d03b4b0bf4b3f37
Katrina Hockman and Noah Schooler <noah@>
2018-10-24 14:30:40 -0700

    Add attempts to loop; failed

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,17 @@
 var vowels = ['a', 'e', 'i', 'o', 'u']
-// var consonants = ['b', 'c', 'd', 'f', 'g', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't']
 
-
-// function split() {
-//   var userInput = "I like ice cream";
-//   var splitUserInput = userInput.split(" ");
-//   console.log(splitUserInput)
-//   debugger;
-// }
-
-var userInput = "i"
+var userInput =
 
 function returnWay(userInput) {
-  if (userInput === "i") {
-    var = ("iway")
+  var returnArray = [];
+
+
+
+
+    if (userInput === "A" || "E" || "I" || "O" || "U") {
+      returnArray.push([i]+ "way");
+    }
   }
-    return var;
-  // var way = [];
-  // for (var i = 0; i < userInput.length; i ++) {
-  //   if (userInput[i] === vowels) {
-  //     way.push(userInput[i])
-  //     way.concat("way")
-  //
-  //   }
-  // }
+    return returnArray;
 
 }
